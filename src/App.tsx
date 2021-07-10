17dc9b070d689ad6b5a7c9048a41f761e300c1b5
juliapp <aragaopintojuli@gmail.com>
2021-07-09 21:38:20 -0300

    integrating mapbox js to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import './style.css';
 
-function App() {
-  return <div className="App"></div>;
+mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_ACCESS_TOKEN
+  ? process.env.REACT_APP_MAP_BOX_ACCESS_TOKEN
+  : '';
+
+//  = process.env.REACT_APP_MAP_BOX_ACCESS_TOKEN;
+
+export default function App() {
+  const container = () => {
+    return <div></div>;
+  };
+  const mapContainer = useRef<any>(container);
+
+  const map = useRef<any>(null);
+  const [lng, setLng] = useState(-55.5503);
+  const [lat, setLat] = useState(-14.2291);
+  const [zoom, setZoom] = useState(4);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom,
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 }
-
-export default App;
