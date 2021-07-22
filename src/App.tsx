ed7582b717f753c89dcae083b5bda4317ffb5c6e
juliapp <aragaopintojuli@gmail.com>
2021-07-22 18:35:39 -0300

    adapting map container

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,5 @@ import Map from './components/Map';
 // import MapComponent from './components/MapComponent';
 
 export default function App() {
-  return (
-    <Map>
-      <div className="sidebar">
-        Longitude: {0} | Latitude: {0} | Zoom: {0}
-      </div>
-    </Map>
-  );
+  return <Map></Map>;
 }
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Map } from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
+import './styles.css';
+const MapLeaflet: React.FC = () => {
+  const [lng, setLng] = useState(-55.0967);
+  const [lat, setLat] = useState(-12.4347);
+  const [zoom, setZoom] = useState(5.2);
+  const [map, setMap] = useState<Map>();
 
-const Map: React.FC = () => {
+  useEffect(() => {
+    map?.on('move', () => {
+      const center = map.getCenter();
+      setLat(parseFloat(center.lat.toFixed(4)));
+      setLng(parseFloat(center.lng.toFixed(4)));
+      setZoom(map.getZoom());
+    });
+  }, [map]);
+
+  const flyToBrazil = () => {
+    map?.flyTo({ lng: -55.0967, lat: -12.4347 }, 5.2);
+  };
+
   return (
     <MapContainer
-      center={[-55.5503, -14.2291]}
-      zoom={4}
-      style={{ width: '100vw', height: '100vh' }}
+      className="map-container"
+      center={[lat, lng]}
+      zoom={zoom}
+      whenCreated={setMap}
     >
       <TileLayer
         // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAP_BOX_ACCESS_TOKEN}`}
       />
+
+      <div className="sidebar forward">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+
+      <button className="button forward" onClick={flyToBrazil}>
+        fly
+      </button>
     </MapContainer>
   );
 };
 
-export default Map;
+export default MapLeaflet;
