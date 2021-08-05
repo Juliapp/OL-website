4b0293858978dfd885c821098bba1b0109d520f5
juliapp <aragaopintojuli@gmail.com>
2021-08-04 22:50:10 -0300

    temporary result showing

--- src/components/Map/MapEventControl.tsx
+++ src/components/Map/MapEventControl.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { icon, LeafletMouseEventHandlerFn } from 'leaflet';
-import { Marker, useMapEvent } from 'react-leaflet';
-import { place } from '@assets';
+import { Marker, Popup, useMapEvent } from 'react-leaflet';
+import { place, iconArea } from '@assets';
 import { useCandidates, useResult } from '@hooks';
 import { HomePageMode } from '../../utils';
 
@@ -13,7 +13,7 @@ const MapEventControl: React.FC<IMapEventControl> = ({
   editCandidates,
   mode,
 }) => {
-  const { onNewCandidate, candidates } = useCandidates();
+  const { onNewCandidate, candidates, onRemoveCandidate } = useCandidates();
   const onClick: LeafletMouseEventHandlerFn = useCallback(
     (e) => {
       // editCandidates && console.log(e.latlng);
@@ -24,31 +24,51 @@ const MapEventControl: React.FC<IMapEventControl> = ({
     [editCandidates, onNewCandidate]
   );
 
-  const results = useResult();
+  const result = useResult();
 
   useMapEvent('click', onClick);
   return (
     <>
-      {mode === HomePageMode.QUERY_FORM &&
-        candidates.able.map((item, index) => {
-          return (
-            <>
-              <Marker
-                eventHandlers={{
-                  click: () => {},
-                }}
-                position={item}
-                key={index}
-                icon={icon({
-                  iconUrl: place,
-                  iconSize: [48, 48],
-                  popupAnchor: [48, 48],
-                  iconAnchor: [23, 40],
-                })}
-              ></Marker>
-            </>
-          );
-        })}
+      {mode === HomePageMode.QUERY_FORM && result
+        ? result.kSolution.map((item, index) => {
+            return (
+              <>
+                <Marker
+                  position={item.candidate}
+                  key={index}
+                  icon={icon({
+                    iconUrl: iconArea,
+                    iconSize: [48, 48],
+                    popupAnchor: [0, -30],
+                    iconAnchor: [23, 40],
+                  })}
+                >
+                  <Popup>{index + 1}</Popup>
+                </Marker>
+              </>
+            );
+          })
+        : candidates.able.map((item, index) => {
+            return (
+              <>
+                <Marker
+                  eventHandlers={{
+                    click: () => {
+                      onRemoveCandidate(index);
+                    },
+                  }}
+                  position={item}
+                  key={index}
+                  icon={icon({
+                    iconUrl: place,
+                    iconSize: [48, 48],
+                    popupAnchor: [48, 48],
+                    iconAnchor: [23, 40],
+                  })}
+                ></Marker>
+              </>
+            );
+          })}
     </>
   );
 };
