38ee6438e84dacfda5217acf2d59709c4699cdc1
juliapp <aragaopintojuli@gmail.com>
2021-08-28 23:20:16 -0300

    this is an exaple of the idea with class as a execution query

--- src/utils/helpers/ExecutionQuery.ts
+++ src/utils/helpers/ExecutionQuery.ts
@@ -1,60 +1,55 @@
-import React from 'react';
-
 interface IExecutionQueryItem {
   id: string;
   percentage?: number;
   label?: string;
 }
 
-const ExecutionQuery = () => {
-  const [query, setQuery] = React.useState<IExecutionQueryItem[]>([]);
-  const [isEmpty, setIsEmpty] = React.useState<boolean>(true);
+export default class ExecutionQuery {
+  query: IExecutionQueryItem[];
+  isEmpty: boolean;
 
-  function _findIndex(item: IExecutionQueryItem): number {
+  constructor() {
+    this.query = [];
+    this.isEmpty = true;
+  }
+
+  _findIndex(item: IExecutionQueryItem): number {
     const { id } = item;
-    return query.findIndex((qitem) => qitem.id === id);
-  }
-  function _findIndexById(id: string) {
-    return query.findIndex((qitem) => qitem.id === id);
+    return this.query.findIndex((qitem) => qitem.id === id);
   }
 
-  function create(item: IExecutionQueryItem) {
-    const index = _findIndex(item);
+  _findIndexById(id: string) {
+    return this.query.findIndex((qitem) => qitem.id === id);
+  }
+
+  create(item: IExecutionQueryItem) {
+    const index = this._findIndex(item);
     if (index === -1) {
-      // this.query.push(item);
-      // this.isEmpty = false;
-      setQuery((previousQuery) => previousQuery.concat(item));
-      if (isEmpty) {
-        setIsEmpty(false);
+      this.query.push(item);
+      this.isEmpty = false;
+    }
+  }
+
+  read() {
+    if (!this.isEmpty) {
+      return this.query[0];
+    }
+  }
+
+  update(item: IExecutionQueryItem) {
+    const index = this._findIndex(item);
+    if (index >= 0) {
+      this.query[index] = item;
+    }
+  }
+
+  delete(id: string) {
+    const index = this._findIndexById(id);
+    if (index >= 0) {
+      this.query.splice(index, 1);
+      if (this.query.length === 0) {
+        this.isEmpty = true;
       }
     }
   }
-
-  function update(item: IExecutionQueryItem) {
-    const index = _findIndex(item);
-    if (index >= 0) {
-      // this.query[index] = item;
-      setQuery((previousQuery) => {
-        previousQuery[index] = item;
-        return previousQuery;
-      });
-    }
-  }
-
-  function del(id: string) {
-    const index = _findIndexById(id);
-    if (index >= 0) {
-      // this.query.splice(index, 1);
-      // if (this.query.length === 0) {
-      //   this.isEmpty = true;
-      // }
-      setQuery((previousQuery) => previousQuery.slice(index, 1));
-      if (query.length === 0) {
-        setIsEmpty(false);
-      }
-    }
-  }
-  return { query, isEmpty, create, update, del };
-};
-
-export default ExecutionQuery;
+}
